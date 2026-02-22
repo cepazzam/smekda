d35922771b51071b17821f312fecea1a0cfb666c
cepazzam <cepazzam80@gmail.com>
2026-02-23 00:47:25 +0700

    edit tabel user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
+            $table->tinyInteger('is_role')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
