4459a425e7870eba060b45106efc57487b47ed7b
cepazzam <cepazzam80@gmail.com>
2026-02-23 00:19:56 +0700

    menambahkan db seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       DB::table('sekolahs')->insert([
+            'name'=>'Cep Azzam',
+            'email'=>'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
