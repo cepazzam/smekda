629f745029cc4af2cbfa90831f5356f6c0277659
cepazzam <cepazzam80@gmail.com>
2026-03-02 02:01:08 +0700

    revisi 4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         $this->call([
+            UserSeeder::class, 
+            CategorySeeder::class, 
+            PhotoSeeder::class,
+           InfoSeeder::class,
+           PostSeeder::class,
+           SekolahSeeder::class,
+           SliderSeeder::class,
+           VideoSeeder::class,
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+
         ]);
     }
 }
